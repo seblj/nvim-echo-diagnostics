c984d225ab0c9934f68e08940b48be93cc760f4d
Sebastian Lyng Johansen <seblyng98@gmail.com>
2021-09-23 14:46:29 +0200

    handle the new diagnostic api and format with stylua

--- lua/echo-diagnostics.lua
+++ lua/echo-diagnostics.lua
@@ -3,38 +3,51 @@ local cmd = vim.cmd
 
 -- Options
 local opt = {
-    show_diagnostic_number = true
+    show_diagnostic_number = true,
 }
 
 M.find_line_diagnostic = function(show_entire_diagnostic)
-    local diagnostics = vim.lsp.diagnostic.get_line_diagnostics()
-    local msg = ""
+    local has_vim_diagnostic = vim.diagnostic ~= nil
+    local diagnostics = {}
+    -- Check if they have the new API for diagnostic and use that if they do.
+    if has_vim_diagnostic then
+        local buffer_diagnostics = vim.diagnostic.get(0)
+        local lnum = vim.fn.line('.')
+        for k, v in pairs(buffer_diagnostics) do
+            if lnum == v.lnum + 1 then
+                table.insert(diagnostics, v)
+            end
+        end
+    else
+        diagnostics = vim.lsp.diagnostic.get_line_diagnostics()
+    end
+    local msg = ''
     if not vim.tbl_isempty(diagnostics) then
         for k, _ in pairs(diagnostics) do
             if diagnostics[k].message then
                 if opt.show_diagnostic_number then
-                    msg = msg .. k .. ": "
+                    msg = msg .. k .. ': '
                 end
                 msg = msg .. diagnostics[k].message
                 if k < #diagnostics then
-                    msg = msg .. "\n"
+                    msg = msg .. '\n'
                 end
             end
         end
         -- Check height of message
         local height = vim.api.nvim_get_option('cmdheight')
-        local tbl = vim.split(msg, "\n")
+        local tbl = vim.split(msg, '\n')
 
         -- Check if we should echo entire diagnostic
         if show_entire_diagnostic then
             if #tbl <= height then
-                msg = msg .. string.rep("\n", (height - #tbl) + 1)
+                msg = msg .. string.rep('\n', (height - #tbl) + 1)
             end
             return msg
         end
 
         if #tbl > height then
-            msg = table.concat(tbl, "\n", 1, height) .. " ..."
+            msg = table.concat(tbl, '\n', 1, height) .. ' ...'
         end
 
         -- Check width of mesasge
@@ -53,7 +66,7 @@ M.echo_line_diagnostic = function()
     if not require('echo-diagnostics').find_line_diagnostic(false) then
         return
     end
-    vim.api.nvim_echo({{require('echo-diagnostics').find_line_diagnostic(false)}}, false, {})
+    vim.api.nvim_echo({ { require('echo-diagnostics').find_line_diagnostic(false) } }, false, {})
     cmd([[autocmd CursorMoved * ++once echo " "]])
 end
 
@@ -61,7 +74,7 @@ M.echo_entire_diagnostic = function()
     if not require('echo-diagnostics').find_line_diagnostic(true) then
         return
     end
-    vim.api.nvim_echo({{require('echo-diagnostics').find_line_diagnostic(true)}}, false, {})
+    vim.api.nvim_echo({ { require('echo-diagnostics').find_line_diagnostic(true) } }, false, {})
     cmd([[autocmd CursorMoved * ++once echo " "]])
 end
 
